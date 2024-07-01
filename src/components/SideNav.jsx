c4def1f7267b502faa29f8d7e4dd675ef73bea07
komalpreet05 <komalbhatia2305@gmail.com>
2024-07-01 13:57:49 -0400

    initial commit

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SideNav = () => {
+    return (
+        <Box
+            display="flex"
+            bg="green"
+        >
+            Sidenav
+        </Box>
+    )
+}
+
+export default SideNav
---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideNav from '../../components/SideNav'
+
+const Dashboard = () => {
+    return (
+        <div>
+            <SideNav></SideNav>
+        </div>
+    )
+}
+
+export default Dashboard
